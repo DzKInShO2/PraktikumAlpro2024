4bcb7716b72884e091e4429a76ae56207530c83e
DzKInShO2 <dzkinsho2@protonmail.com>
2024-03-24 13:40:13 +0800

    Add some content.

--- res/m1c1.cpp
+++ res/m1c1.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
     float temp;
 
-    std::cout << "Suhu awal: ";
-    std::cin >> temp;
-    std::cout << std::endl;
+    cout << "Suhu awal: ";
+    cin >> temp;
+    cout << endl;
 
-    std::cout << "Suhu awal kamar Holil: " << std::endl;
-    std::cout << "1. " << temp << " C" << std::endl;
-    std::cout << "2. " << ((temp * 1.8f) + 32) << " F" << std::endl;
-    std::cout << "3. " << (temp + 273.15f) << " K" << std::endl << std::endl;
+    cout << "Suhu awal kamar Holil: " << endl;
+    cout << "1. " << temp << " C" << endl;
+    cout << "2. " << ((temp * 1.8f) + 32) << " F" << endl;
+    cout << "3. " << (temp + 273.15f) << " K" << endl << endl;
 
     float temp0 = (temp - (temp * 0.25f));
-    std::cout << "Setelah 15 menit" << std::endl;
-    std::cout << "Suhu kamar Holil berkurang menjadi " << ((temp0 * 1.8f) + 32) << " F dengan pengurangan sebesar " << ((temp - temp0) + 273.15f) << " K" << std::endl;
+    cout << "Setelah 15 menit" << endl;
+    cout << "Suhu kamar Holil berkurang menjadi " << ((temp0 * 1.8f) + 32)
+         << " F dengan pengurangan sebesar " << ((temp - temp0) + 273.15f)
+         << " K" << endl;
 
     float temp1 = (temp - (temp * 0.65f));
-    std::cout << "Setelah 25 menit" << std::endl;
-    std::cout << "Suhu kamar Holil berkurang menjadi " << (temp1 + 273.15f) << " K dengan pengurangan sebesar " << (temp - temp1) << " C" << std::endl << std::endl;
+    cout << "Setelah 45 menit" << endl;
+    cout << "Suhu kamar Holil berkurang menjadi " << (temp1 + 273.15f)
+         << " K dengan pengurangan sebesar " << (temp - temp1) << " C"
+         << endl << endl;
 
-    std::cout << "Suhu awal kamar Holil: " << temp << " C" << std::endl;
-    std::cout << "Suhu kamar Holil saat ini: " << temp1 << " C" << std::endl << std::endl;
+    cout << "Suhu awal kamar Holil: " << temp << " C" << endl;
+    cout << "Suhu kamar Holil saat ini: " << temp1 << " C" <<endl<<endl;
 
-    std::cout << "Holil mematikan pendingin..." << std::endl << std::endl;
+    cout << "Holil mematikan pendingin..." << endl << endl;
 
     float temp2 = (temp1 + (temp1 * 0.35f));
-    std::cout << "Setelah 25 menit" << std::endl;
-    std::cout << "Suhu kamar Holil naik menjadi " << temp2 << " C" << std::endl << std::endl;
+    cout << "Setelah 25 menit" << endl;
+    cout << "Suhu kamar Holil naik menjadi " << temp2 <<" C"<<endl<<endl;
 
-    std::cout << "Hasil perbandingan :" << std::endl;
-    std::cout << "1:" << (temp/temp2) << " untuk suhu saat ini" << std::endl;
-    std::cout << "1:" << (temp/temp1) << " untuk suhu terdingin" << std::endl;
-    std::cout << "1:" << (temp/temp0) << " untuk suhu 15 menit setelah pendinginan" << std::endl;
+    cout << "Hasil perbandingan :" << endl;
+    cout << "1:" << (temp/temp2) << " untuk suhu saat ini" << endl;
+    cout << "1:" << (temp/temp1) << " untuk suhu terdingin" << endl;
+    cout << "1:" << (temp/temp0)
+         << " untuk suhu 15 menit setelah pendinginan" << endl;
 
     return 0;
 }
