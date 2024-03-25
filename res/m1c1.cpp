408b971bc90a0ef375d11ef699f92abfc41ee296
DzKInShO2 <dzkinsho2@protonmail.com>
2024-03-25 08:10:27 +0800

    Revise m1p1, m1p4, and redraw m1f1

--- res/m1c1.cpp
+++ res/m1c1.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main() {
     float temp;
-
     cout << "Suhu awal: ";
     cin >> temp;
     cout << endl;
@@ -11,23 +10,19 @@ int main() {
     cout << "1. " << temp << " C" << endl;
     cout << "2. " << ((temp * 1.8f) + 32) << " F" << endl;
     cout << "3. " << (temp + 273.15f) << " K" << endl << endl;
-
     float temp0 = (temp - (temp * 0.25f));
     cout << "Setelah 15 menit" << endl;
     cout << "Suhu kamar Holil berkurang menjadi " << ((temp0 * 1.8f) + 32)
          << " F dengan pengurangan sebesar " << ((temp - temp0) + 273.15f)
          << " K" << endl;
-
     float temp1 = (temp - (temp * 0.65f));
     cout << "Setelah 45 menit" << endl;
     cout << "Suhu kamar Holil berkurang menjadi " << (temp1 + 273.15f)
          << " K dengan pengurangan sebesar " << (temp - temp1) << " C"
          << endl << endl;
-
     cout << "Suhu awal kamar Holil: " << temp << " C" << endl;
     cout << "Suhu kamar Holil saat ini: " << temp1 << " C" <<endl<<endl;
     cout << "Holil mematikan pendingin..." << endl << endl;
-
     float temp2 = (temp1 + (temp1 * 0.35f));
     cout << "Setelah 25 menit" << endl;
     cout << "Suhu kamar Holil naik menjadi " << temp2 <<" C"<<endl<<endl;
