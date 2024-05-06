2f953c1bd0f4180bc096f90ea083268ed647a5fa
DzKInShO2 <dzkinsho2@protonmail.com>
2024-05-06 08:56:08 +0800

    m3p4:1

--- res/m3c1.cpp
+++ res/m3c1.cpp
@@ -20,19 +20,3 @@ int main() {
         matrix[i * m + j] += (int)(numbers[k] - '0') * base;
         base *= 10;
       }
-      for (int k = mid + 1; k < l; ++k) { 
-        if (numbers[k] != ' ') break; mid = k;
-      }
-    }
-  }
-  cout << "\nHasil rotasi 90 derajat searah jarum jam:" << endl;
-  for (int i = 0; i < n; ++i) {
-    for (int j = m - 1; j >= 0; --j) cout <<matrix[j * m + i]<< " ";
-    cout << endl; 
-  }
-  cout << "\nHasil pencerminan terhadap sumbu X:\n";
-  for (int i = n - 1; i >= 0; --i) {
-    for (int j = m - 1; j >= 0; --j) cout <<matrix[j * m + i]<< " ";
-    cout << endl;
-  }
-}
