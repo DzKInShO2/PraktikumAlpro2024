18361fb9779c128c2754bf9ed33657330fd4dda5
DzKInShO2 <dzkinsho2@protonmail.com>
2024-06-06 04:44:29 +0800

    m2p4: finished!

--- res/m2c1a.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-  cout << "------------------------------------------\n";
-  cout << "|    THR DARI HOLIL UNTUK SEMUA  ^_^     |\n";
--- res/m2c2a.cpp
+++
@@ -1,5 +0,0 @@
-#include <conio.h>
-#include <iostream>
-using namespace std;
-int main() {
-  char state = 0; int n0 = 0; int n1 = 0;
